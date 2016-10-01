007426db1e8c6e6ad3562ceba84c0ff0b3f69145
Chad Walter <cwalter9@gmail.com>
2016-10-01 18:15:52 -0400

    Updated MySQL schema

--- internetscan.sql
+++ internetscan.sql
@@ -29,8 +29,22 @@ CREATE TABLE `address` (
   `adddate` date DEFAULT NULL,
   `updown` int(3) DEFAULT NULL,
   PRIMARY KEY (`addid`,`oct1`),
-  UNIQUE KEY `IPADDR` (`oct1`,`oct2`,`oct3`,`oct4`)
-) ENGINE=InnoDB AUTO_INCREMENT=5862 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `IPADDR` (`oct1`,`oct2`,`oct3`,`oct4`),
+  UNIQUE KEY `idaddress` (`addid`,`ipaddr`),
+  UNIQUE KEY `addstat` (`addid`,`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=205965 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `log` */
+
+DROP TABLE IF EXISTS `log`;
+
+CREATE TABLE `log` (
+  `id` int(240) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(16) NOT NULL,
+  `entry` varchar(240) DEFAULT NULL,
+  `ltime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=45525 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `ports` */
 
@@ -40,9 +54,36 @@ CREATE TABLE `ports` (
   `poid` int(250) NOT NULL AUTO_INCREMENT,
   `addid` int(250) NOT NULL,
   `port` int(8) DEFAULT NULL,
+  `status` int(4) DEFAULT '0',
   `chtime` int(12) DEFAULT NULL,
-  PRIMARY KEY (`poid`,`addid`)
-) ENGINE=InnoDB AUTO_INCREMENT=898 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`poid`,`addid`),
+  UNIQUE KEY `idportstat` (`poid`,`port`,`status`),
+  UNIQUE KEY `statidadd` (`poid`,`addid`,`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=68496 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `service` */
+
+DROP TABLE IF EXISTS `service`;
+
+CREATE TABLE `service` (
+  `sid` int(250) NOT NULL AUTO_INCREMENT,
+  `addid` int(250) NOT NULL,
+  `poid` int(250) NOT NULL,
+  `service` varchar(64) DEFAULT NULL,
+  `tstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sid`,`addid`,`poid`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `vars` */
+
+DROP TABLE IF EXISTS `vars`;
+
+CREATE TABLE `vars` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `vname` varchar(64) DEFAULT NULL,
+  `vfield` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `hostports` */
 
@@ -57,12 +98,33 @@ DROP TABLE IF EXISTS `hostports`;
  `port` int(8) 
 )*/;
 
+/*Table structure for table `vw_services` */
+
+DROP TABLE IF EXISTS `vw_services`;
+
+/*!50001 DROP VIEW IF EXISTS `vw_services` */;
+/*!50001 DROP TABLE IF EXISTS `vw_services` */;
+
+/*!50001 CREATE TABLE  `vw_services`(
+ `Service` varchar(64) ,
+ `Address` varchar(20) ,
+ `PORT` int(8) ,
+ `Time Stamp` timestamp 
+)*/;
+
 /*View structure for view hostports */
 
 /*!50001 DROP TABLE IF EXISTS `hostports` */;
 /*!50001 DROP VIEW IF EXISTS `hostports` */;
 
 /*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`admincw`@`%` SQL SECURITY DEFINER VIEW `hostports` AS select `address`.`addid` AS `addid`,`address`.`ipaddr` AS `ipaddr`,`ports`.`port` AS `port` from (`address` left join `ports` on((`address`.`addid` = `ports`.`addid`))) */;
+
+/*View structure for view vw_services */
+
+/*!50001 DROP TABLE IF EXISTS `vw_services` */;
+/*!50001 DROP VIEW IF EXISTS `vw_services` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`admincw`@`%` SQL SECURITY DEFINER VIEW `vw_services` AS select `service`.`service` AS `Service`,`address`.`ipaddr` AS `Address`,`ports`.`port` AS `PORT`,`service`.`tstamp` AS `Time Stamp` from (`service` left join (`address` join `ports`) on(((`address`.`addid` = `service`.`addid`) and (`ports`.`poid` = `service`.`poid`)))) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
